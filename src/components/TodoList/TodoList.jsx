f6d744bce308142ee6911a8d7c74f34471619b42
geelona <mr.geelona@gmail.com>
2023-04-28 16:45:36 +0300

    added Edit button functionality

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 function TodoList({ todo, setTodo }) {
+
+  const [edit, setEdit] = useState(null)
+  const [value, setValue] = useState('')
 
   function deleteTodo(id) {
     let newTodo = [...todo].filter(item => item.id != id)
@@ -17,14 +20,46 @@ function TodoList({ todo, setTodo }) {
     setTodo(newTodo)
   }
 
+  function editTodo(id, title) {
+    setEdit(id)
+    setValue(title)
+  }
+
+  function saveTodo(id) {
+    let newTodo = [...todo].map(item => {
+      if (item.id == id) {
+        item.title = value
+      }
+      return item
+    })
+    setTodo(newTodo)
+    setEdit(null)
+  }
+
   return (
     <div>
       {
         todo.map( item => (
           <div key={item.id}>
-            <div>{item.title}</div>
-            <button onClick={() => deleteTodo(item.id) }>Delete</button>
-            <button onClick={() => statusTodo(item.id) }>Close task / Open task</button>
+            {
+              edit == item.id ?
+              <div>
+                <input value={value} onChange={(e) => setValue(e.target.value)}/>
+              </div> :
+              <div>{ item.title }</div>
+            }
+            {
+              edit == item.id ?
+                <div>
+                  <button onClick={ () => saveTodo(item.id)}>Save</button>
+                </div> :
+                <div>
+                  <button onClick={ () => deleteTodo(item.id) }>Delete</button>
+                  <button onClick={ () => editTodo(item.id, item.title) }>Edit</button>
+                  <button onClick={ () => statusTodo(item.id) }>Close task / Open task</button>
+                </div>
+            }
+
           </div>
         ))
       }
